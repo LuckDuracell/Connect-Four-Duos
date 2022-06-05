ee3610359d01b1b323cd5f2168562f43ae3ca86d
LuckDuracell <lukedru@yahoo.com>
2022-06-05 12:07:23 -0700

    Initial Commit

---
+++ Connect Four Duos/Connect_Four_DuosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Connect_Four_DuosApp.swift
+//  Connect Four Duos
+//
+//  Created by Luke Drushell on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Connect_Four_DuosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
